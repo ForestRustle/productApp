ec77db0ccd864e03450c54d8795c00979a4341c3
ForestRustle <fenix66690@gmail.com>
2025-11-04 18:20:31 +0300

    fix - Проблема с редиретом на страницу

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
 'use client';
 
 import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
 
 export default function Home() {
-  const router = useRouter();
-
   useEffect(() => {
-    router.push('/products');
-  }, [router]);
+    window.location.href = '/products/';
+  }, []);
 
   return (
     <div
@@ -17,9 +14,11 @@ export default function Home() {
         justifyContent: 'center',
         alignItems: 'center',
         height: '100vh',
+        fontSize: '18px',
+        color: '#666',
       }}
     >
-      <div>Перенаправление на страницу продуктов...</div>
+      Перенаправление на страницу продуктов...
     </div>
   );
 }
